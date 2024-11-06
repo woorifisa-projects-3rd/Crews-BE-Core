f8e07a0d2d316634e9b2d3d8e00c365a50117983
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-11-06 15:21:06 +0900

    feat: Project Setting

---
+++ src/main/java/org/baas/baascore/BaascoreApplication.java
@@ -0,0 +1,13 @@
+package org.baas.baascore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaascoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaascoreApplication.class, args);
+    }
+
+}
