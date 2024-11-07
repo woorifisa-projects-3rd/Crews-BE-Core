ff55464fde0c54ea9e1d017ea0e10f4b2a30c0eb
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-11-07 15:12:19 +0900

    refact : bankCode 누락 해결

--- src/main/java/org/baas/baascore/model/Bank.java
+++ src/main/java/org/baas/baascore/model/Bank.java
@@ -2,6 +2,7 @@ package org.baas.baascore.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.baas.baascore.util.BaseTimeEntity;
 
 /**
  * 은행 정보를 저장하는 BankCode 엔티티
@@ -9,11 +10,15 @@ import lombok.Getter;
 @Getter
 @Entity
 @Table(name = "bank")
-public class Bank {
+public class Bank extends BaseTimeEntity {
     // 은행 정보의 고유 식별자
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // 은행 코드 (예: 020, 090)
+    @Column(name = "code", nullable = false)
+    private String bankCode;
 
     // 은행 이름 (예: 하나은행, 국민은행)
     @Column(name = "name", nullable = false)
