f1302c02c1b78044068124804c007be09dad81fa
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-11-07 16:33:00 +0900

    feat : SecurityUtils(유틸클래스) 추가

---
+++ src/main/java/org/baas/baascore/util/SecurityUtils.java
@@ -0,0 +1,50 @@
+package org.baas.baascore.util;
+
+import lombok.RequiredArgsConstructor;
+import org.baas.baascore.excaption.ErrorCode;
+import org.baas.baascore.excaption.HashingAlgorithmNotFoundException;
+import org.baas.baascore.model.Subscribe;
+import org.baas.baascore.repository.SubscribeRepository;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class SecurityUtils {
+    private SecurityUtils() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+    private final SubscribeRepository subscribeRepository;
+
+    // SHA-256 해시화 메서드
+    public static String hashSecretKey(String secretKey) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(secretKey.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new HashingAlgorithmNotFoundException(ErrorCode.HASH_ALGORITHM_NOT_FOUND);
+        }
+    }
+
+    // 사업자등록번호 유효성 검증 메서드
+    public static boolean isValidBusinessNumber(String businessNum) {
+        return businessNum != null && businessNum.matches("\\d{10}");
+    }
+
+    // API 키와 시크릿 키 검증 메서드
+    public static boolean validateKeys(String accessKey, String secretKey, SubscribeRepository subscribeRepository) {
+        Optional<Subscribe> optionalSubscribe = subscribeRepository.findByAccessKey(accessKey);
+
+        if (optionalSubscribe.isPresent()) {
+            Subscribe subscribe = optionalSubscribe.get();
+            String hashedInputSecretKey = hashSecretKey(secretKey);
+            return subscribe.getSecretKeyHash().equals(hashedInputSecretKey);
+        }
+
+        return false;
+    }
+}
